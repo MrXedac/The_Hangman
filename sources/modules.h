0f1f45b8b37fab4a87d142bab80734e41569e719
Quentin Bergougnoux <quentin.bergougnoux@gmail.com>
2016-08-30 13:42:04 +0200

    Added .gitignore. Updated ascii art code by using a static char array, one-ligning the draw method.

--- sources/modules.h
+++ sources/modules.h
@@ -9,4 +9,17 @@ void difficultyChoice(uint32_t *length, uint32_t *MIN, uint32_t *MAX);
 void rules();
 void hanged(uint32_t i);
 
+static const char* const hangedPic[] = {
+	"\n\n\n\n\n\n\n\n\n\t|\t\t |\n\n",
+	"\n\n\n\n\n\n\n\n\t------------------\n\t|\t\t |\n\n",
+	"\n\n\n\n\n\t\n\t|\t\t\n\t|\t\t\n\t------------------\n\t|\t\t |\n\n",
+	"\n\t\n\t\t \n\t\t \n\t|\t\n\t|\t \n\t|\t\t\n\t|\t\t\n\t------------------\n\t|\t\t |\n\n",
+	"\n\t\n\t|\t \n\t|\t \n\t|\t\n\t|\t\n\t|\t\t\n\t|\t\t\n\t------------------\n\t|\t\t |\n\n",
+	"\n\t----------\n\t|\t \n\t|\t \n\t|\t\n\t|\t \n\t|\t\t\n\t|\t\t\n\t------------------\n\t|\t\t |\n\n",
+	"\n\t----------\n\t|\t |\n\t|\t \n\t|\t\n\t|\t \n\t|\t\t\n\t|\t\t\n\t------------------\n\t|\t\t |\n\n",
+	"\n\t----------\n\t|\t |\n\t|\t O\n\t|\t\n\t|\t \n\t|\t\t\n\t|\t\t\n\t------------------\n\t|\t\t |\n\n",
+	"\n\t----------\n\t|\t |\n\t|\t O\n\t|\t-+-\n\t|\t \n\t|\t\t\n\t|\t\t\n\t------------------\n\t|\t\t |\n\n",
+	"\n\t----------\n\t|\t |\n\t|\t O\n\t|\t-+-\n\t|\t/ \\ \n\t|\t\t\n\t|\t\t\n\t------------------\n\t|\t\t |\n\n\t===YOU ARE DEAD===\n\n",
+};
+
 #endif /* FONCTIONS_H_INCLUDED */
